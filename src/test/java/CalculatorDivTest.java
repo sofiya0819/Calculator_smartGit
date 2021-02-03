b5f2f3eed9442a3bca1219ccf16393398e757876
sofiya0819 <sofiya0819@gmail.com>
2021-02-03 18:19:56 +0200

    Adding a test when divide numbers with 3 digits after decimal point.

--- src/test/java/CalculatorDivTest.java
+++ src/test/java/CalculatorDivTest.java
@@ -95,6 +95,18 @@ public class CalculatorDivTest {
         assertEquals(res, cal.divide(a,b),0.1);
     }
 
+    @Test(expected = InvalidParameterException.class)
+    public void DividePositiveNumbersWithTreeDigitsAfterDecimalPoint(){
+        System.out.println("Divide zero by zero ");
+        double a = 6.555;
+        double b = 5.999;
+//        double res = 1.092682114;
+        double res = 1.093;
+        //depricated with 2 params
+        assertEquals(res, cal.divide(a,b),0.001);
+    }
+
+
     @AfterClass
     public static void tearDown(){
         System.out.println("Test finished");
